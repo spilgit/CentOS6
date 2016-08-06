4b9df3a3309cd2402c1f88bf19ad683a53792954
K Farrow <spilgit@users.noreply.github.com>
2016-08-05 22:43:45 -0400

    Update freeipa.sh

--- freeipa.sh
+++ freeipa.sh
@@ -3,11 +3,12 @@
 #
 yum -y install php-ldap php-apc php-mbstring php-dom php-soap php-xmlrpc
 yum -y install ipa-server bind bind-dyndb-ldap
+yum -y install mod_auth_kerb
 #
 nano /etc/hosts
 # 127.0.0.1 localhosts
 # ::1       localhosts
-# IP ADDRESS website.com website
+# IP ADDRESS master.website.com
 #
 # To INSTALL
 ipa-server-install --setup-dns
