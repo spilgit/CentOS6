16767857de3b8db01ca35325534c0169262bd985
K Farrow <spilgit@users.noreply.github.com>
2016-08-05 22:35:06 -0400

    Update freeipa.sh

--- freeipa.sh
+++ freeipa.sh
@@ -3,4 +3,13 @@
 # This will help you Setup FreeIPA for CentOS 6
 # Author; K Farrow (khmf1@outlook.com)
 #
+nano /etc/hosts
+# 127.0.0.1 localhosts
+# ::1       localhosts
+# IP ADDRESS website.com website
+#
+# To INSTALL
 ipa-server-install --setup-dns
+# To Un-install
+ipa-server-install --uninstall
+
