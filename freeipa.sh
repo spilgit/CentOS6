d53a70c409513e7de3b12587f1d8b0f85886dfe5
K Farrow <spilgit@users.noreply.github.com>
2016-08-10 09:20:14 -0400

    Update freeipa.sh

--- freeipa.sh
+++ freeipa.sh
@@ -4,6 +4,7 @@
 yum -y install php-ldap php-apc php-mbstring php-dom php-soap php-xmlrpc
 yum -y install ipa-server bind bind-dyndb-ldap
 yum -y install mod_auth_kerb
+yum install glibc glibc-devel gcc python* libacl-devel krb5-workstation krb5-libs pam_krb5 git-core openldap-devel
 #
 nano /etc/hosts
 # 127.0.0.1 localhosts
@@ -14,7 +15,6 @@ nano /etc/hosts
 ipa-server-install --setup-dns
 # To Un-install
 ipa-server-install --uninstall
-
 # Next steps:
 #        1. You must make sure these network ports are open:
 #                TCP Ports:
@@ -26,4 +26,7 @@ ipa-server-install --uninstall
 #                  * 88, 464: kerberos
 #                 * 53: bind
 #                  * 123: ntp
-
+nano /etc/sysconfig/network-scripts/ifcfg-eth0
+nano /etc/resolv.conf
+nano /etc/sysconfig/network
+nano /etc/hosts
