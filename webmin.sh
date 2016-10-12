b2f0caff0fc2695654875745844938314ff28ceb
KHMF Technologies <spilgit@users.noreply.github.com>
2016-10-12 09:16:01 -0400

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,3 +1,7 @@
 # Webmin from Source
 cd /tmp
 wget https://sourceforge.net/projects/webadmin/files/webmin/1.820/webmin-1.820.tar.gz
+gunzip webmin-1.820.tar.gz
+cd webmin-1.820
+./setup.sh /usr/local/webmin
+
