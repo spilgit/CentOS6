1067f568bc5235c625c71d9ed7f2c470333935ca
KHMF Technologies <spilgit@users.noreply.github.com>
2016-10-12 09:17:39 -0400

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -4,4 +4,8 @@ wget https://sourceforge.net/projects/webadmin/files/webmin/1.820/webmin-1.820.t
 gunzip webmin-1.820.tar.gz
 cd webmin-1.820
 ./setup.sh /usr/local/webmin
+# Note:  Open Port 10000
+# http://localhost:10000
+
+
 
